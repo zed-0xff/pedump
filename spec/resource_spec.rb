dfa230492ad070a693737a37ec3ebaa5f842ee1a
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2011-12-14 19:30:35 +0400

    resource spec

---
+++ spec/resource_spec.rb
@@ -0,0 +1,13 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../lib/pedump')
+
+describe PEdump do
+  it "should get all resources" do
+    fname = File.expand_path(File.dirname(__FILE__) + '/../samples/calc.exe')
+    File.open(fname,"rb") do |f|
+      @pedump = PEdump.new(fname)
+      @resources = @pedump.resources(f)
+    end
+    @resources.size.should == 71
+  end
+end
