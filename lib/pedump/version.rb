dc596fd3da4e88a4d3471890eb99737fbcb19da9
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2011-12-18 00:20:09 +0400

    Version bump to 0.4.0

--- lib/pedump/version.rb
+++ lib/pedump/version.rb
@@ -1,8 +1,8 @@
 class PEdump
   module Version
     MAJOR = 0
-    MINOR = 3
-    PATCH = 3
+    MINOR = 4
+    PATCH = 0
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
