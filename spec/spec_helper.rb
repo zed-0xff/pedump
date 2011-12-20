225e23dc8026a41df5b91d8abe0677fd639b1e05
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2011-12-20 08:17:16 +0400

    simplify specs, add virtsectblXP.exe

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,5 +8,15 @@ require 'pedump'
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
-  
+end
+
+def sample
+  @pedump ||=
+    begin
+      fname = self.example.full_description.split.first
+      fname = File.expand_path(File.dirname(__FILE__) + '/../samples/' + fname)
+      File.open(fname,"rb") do |f|
+        PEdump.new(fname).dump
+      end
+    end
 end
---
+++ spec/virtsectblXP_spec.rb
@@ -0,0 +1,12 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../lib/pedump')
+
+describe 'virtsectblXP.exe' do
+  it "should have 2 imports" do
+    sample.imports.size.should == 2
+    sample.imports.map(&:module_name).should == %w'kernel32.dll msvcrt.dll'
+    sample.imports.map do |iid|
+      (iid.original_first_thunk + iid.first_thunk).uniq.map(&:name)
+    end.flatten.should == ["ExitProcess", "printf"]
+  end
+end
