b2c8acfedf7eeec4c9f8fe97676d63449e2b1dd3
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2011-12-20 12:01:04 +0400

    don't run slow specs by default

--- spec/sig_all_packers_spec.rb
+++ spec/sig_all_packers_spec.rb
@@ -3,18 +3,22 @@ require File.expand_path(File.dirname(__FILE__) + '/../lib/pedump/packer')
 
 describe "PEdump::Packer" do
   describe "matchers" do
-    PEdump::SigParser.parse(:raw => true).each do |sig|
-      data = sig.re.join
-      next if data == "This program cannot be run in DOS mo"
-      it "should find #{sig.name}" do
-        a = PEdump::Packer.of(data).map(&:name)
-        a.size.should > 0
+    if ENV['SLOW']
+      PEdump::SigParser.parse(:raw => true).each do |sig|
+        data = sig.re.join
+        next if data == "This program cannot be run in DOS mo"
+        it "should find #{sig.name}" do
+          a = PEdump::Packer.of(data).map(&:name)
+          a.size.should > 0
 
-        a = sig.name.split - a.join(' ').split - ['Exe','PE']
-        a.delete_if{ |x| x[/[vV\.\/()\[\]]/] }
-        p a if a.size > 1
-        a.size.should < 2
+          a = sig.name.split - a.join(' ').split - ['Exe','PE']
+          a.delete_if{ |x| x[/[vV\.\/()\[\]]/] }
+          p a if a.size > 1
+          a.size.should < 2
+        end
       end
+    else
+      pending "SLOW"
     end
   end
 end
