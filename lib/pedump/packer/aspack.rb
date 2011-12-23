2fee079d1d7f353b0a9a837d810032e50eecf619
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2011-12-23 18:20:04 +0400

    ASPack: detect by RE & LZX table

--- lib/pedump/packer/aspack.rb
+++ lib/pedump/packer/aspack.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# coding: binary
 require 'pedump'
 require 'pedump/packer'
 require 'zlib' # for crc32
@@ -12,13 +13,13 @@ class PEdump::Packer::ASPack
   # thanks to Dr.Golova for ASPack Unpacker v1.00
 
   ASPACK_INFO = Struct.new(
-    :Crc1Ofs,                      # first checksum offset
-    :Crc1Len,                      # first checksum length
-    :Crc1Val,                      # first checksum value
-    :Crc2Ofs,                      # second checksum offset
-    :Crc2Len,                      # second checksum length
-    :Crc2Val,                      # second checksum value
-    :VerName,                      # name of this version
+    :Crc1Ofs,                      # crc1 offset
+    :Crc1Len,                      # crc1 length
+    :Crc1Val,                      # crc1 value
+    :Crc2Ofs,                      #   crc2 offset
+    :Crc2Len,                      #   crc2 length
+    :Crc2Val,                      #   crc2 value
+    :version,                      # ASPack version name
     :ObjTbl,                       # object table offset
     :FlgE8E9,                      # e8/e9 filter flag offset
     :ModE8E9,                      # e8/e9 filter mode offset
@@ -30,12 +31,12 @@ class PEdump::Packer::ASPack
 
   AspInfos = [
     ASPACK_INFO.new(
-      0x39e,                   # first checksum offset
-      0x21,                    # first checksum length
-      0x98604df5,              # first checksum value
-      0x9d,                    # second checksum offset
-      0x62,                    # second checksum length
-      0xa82446ae,              # second checksum value
+      0x39e,                   # crc1 offset
+      0x21,                    # crc1 length
+      0x98604df5,              # crc1 value
+      0x9d,                    #   crc2 offset
+      0x62,                    #   crc2 length
+      0xa82446ae,              #   crc2 value
       "v2.12",                 # name of this version
       0x57b,                   # object table offset
       0xfe,                    # e8/e9 filter flag offset
@@ -46,6 +47,52 @@ class PEdump::Packer::ASPack
       0x39a                    # offset of entry point rva
     )
   ]
+
+# 0x442a4e = ep       = 0x4150d2
+# 0x4429ac = e8_flag
+
+#    004151CE 8B 9D ?? ?? ?? ??           mov     ebx, [ebp+442A3Eh]
+#    004151D4 0B DB                       or      ebx, ebx
+#    004151D6 74 0A                       jz      short crc2_start
+#    004151D8 8B 03                       mov     eax, [ebx]
+#    004151DA 87 85 ?? ?? ?? ??           xchg    eax, [ebp+442A42h]
+#    004151E0 89 03                       mov     [ebx], eax
+#
+#    004151E2 8D B5 ?? ?? ?? ??           lea     esi, [ebp+442A5Ah]
+#    004151E8 83 3E 00                    cmp     dword ptr [esi], 0
+#    004151EB 0F 84 1F 01 00 00           jz      loc_415310
+#    004151F1 8D B5 ?? ?? ?? ??           lea     esi, [ebp+442A5Ah]
+#    004151F7 6A 04                       push    4
+#    004151F9 68 00 10 00 00              push    1000h
+#    004151FE 68 00 18 00 00              push    1800h
+#    00415203 6A 00                       push    0
+#    00415205 FF 95 ?? ?? ?? ??           call    dword ptr [ebp+4429B9h]
+#    0041520B 89 85 ?? ?? ?? ??           mov     [ebp+4429B5h], eax
+#
+#    00415211 8B 46 04                    mov     eax, [esi+4]
+#    00415214 05 0E 01 00 00              add     eax, 10Eh
+#    00415219 6A 04                       push    4
+#    0041521B 68 00 10 00 00              push    1000h
+#    00415220 50                          push    eax
+#    00415221 6A 00                       push    0
+#    00415223 FF 95 ?? ?? ?? ??           call    dword ptr [ebp+4429B9h]
+#    00415229 89 85 ?? ?? ?? ??           mov     [ebp+4429B1h], eax
+#    0041522F 56                          push    esi
+#    00415230 8B 1E                       mov     ebx, [esi]
+#    00415232 03 9D ?? ?? ?? ??           add     ebx, [ebp+4437E0h]
+#    00415238 FF B5 ?? ?? ?? ??           push    dword ptr [ebp+4429B5h]
+#    0041523E FF 76 04                    push    dword ptr [esi+4]
+#    00415241 50                          push    eax
+#    00415242 53                          push    ebx
+#    00415243 E8 3B 03 00 00              call    sub_415583
+#    00415248 80 BD ?? ?? ?? ?? 00        cmp     byte ptr [ebp+4429ACh], 0
+  RE = /
+    \x8B\x9D....\x0B\xDB\x74\x0A\x8B\x03\x87\x85....\x89\x03\x8D\xB5....\x83
+    \x3E\x00\x0F\x84\x1F\x01\x00\x00\x8D\xB5....\x6A\x04\x68\x00\x10\x00\x00
+    \x68\x00\x18\x00\x00\x6A\x00\xFF\x95....\x89\x85....\x8B\x46\x04\x05\x0E
+    \x01\x00\x00\x6A\x04\x68\x00\x10\x00\x00\x50\x6A\x00\xFF\x95....\x89\x85
+    ....\x56\x8B\x1E\x03\x9D....\xFF\xB5....\xFF\x76\x04\x50\x53\xE8\x3B\x03
+    \x00\x00\x80\xBD....\x00/mx
 
   ASP_OBJ = PEdump.create_struct 'V2', :va, :size
 
@@ -65,12 +112,16 @@ class PEdump::Packer::ASPack
 
       f.seek ep_file_offset
       @ep_code = f.read(EP_CODE_SIZE)
-      @info = find_version
     end
   end
 
   # detect used ASPack version
   def find_version
+    @info = _find_version
+  end
+
+  # detect used ASPack version
+  def _find_version
     logger.debug "[.] uMaxOfs = #@uMaxOfs"
     AspInfos.each do |info|
       #logger.debug "[.] info = #{info.inspect}"
@@ -90,12 +141,83 @@ class PEdump::Packer::ASPack
       crc = Zlib.crc32(@ep_code[info.Crc2Ofs, info.Crc2Len])
       next if crc ^ info.Crc2Val != 0xffff_ffff
 
+      logger.info "[.] detected ASPack #{info.version}"
       return info
     end
+
+    if pos = (@ep_code =~ RE)
+      logger.info "[.] RE found at offset 0x%x" % pos
+      info = ASPACK_INFO.new
+      info.version = "v2.1"
+      info.ObjTbl  = pos - 0xf0
+      info.OepOfs  = pos - 0xfc
+      info.ImpTbl  = pos - 0x100
+      info.FlgE8E9 = pos - 0x19e
+      info.ModE8E9 = pos + 0xc3
+      info.CmpE8E9 = pos + 0xc6
+      logger.info "[.] detected ASPack #{info.version}"
+      return info
+    end
+
+    # try to find UNLZX table
+    t = find_by_unlzx
+    return t if t
 
     logger.fatal "[!] unknown ASPack version, or not ASPack at all!"
 
     # not found
+    nil
+  end
+
+  def find_by_unlzx
+    # LzxTblLenBase
+    a = [ 0,  1,  2,  3,  4,  5,   6,   7,   8,  10,  12, 14, 16, 20, 24, 28, 32, 40, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224]
+    re = Regexp.new(a.map{ |x| "\\x%02x" % x}.join.force_encoding('binary'), Regexp::MULTILINE)
+    if pos = (@ep_code =~ re)
+      logger.info "[.] found LzxTblLenBase at 0x%x" % pos
+      s = "".force_encoding('binary')
+      10.times{ s << 0 } # string of 10 NULL characters
+      pos -= 2 while pos > 0 && @ep_code[pos,10] != s
+      if pos <= 0
+        logger.error "[!] failed to find block of zeroes before LzxTblLenBase"
+        return nil
+      end
+      pos0 = pos
+      pos-=1 while pos > 0 && @ep_code[pos] == "\x00"
+      if pos <= 0
+        logger.error "[!] can't find nonzero bytes before zeroes"
+        return nil
+      end
+      zeroes_start = pos+1
+      pos = pos0
+      pos+=1 while pos < @ep_code.size && @ep_code[pos] == "\x00"
+      logger.debug "[.] found block of 0x%x zeroes starting at 0x%x" % [pos-zeroes_start, zeroes_start]
+      pos = zeroes_start
+      pos0 = pos
+
+      found = false
+      while pos > 0 && (pos0-pos < 0xff)
+        a = @ep_code[pos,4*4].unpack('V*')
+        if(
+           a[0] == @pe.sections[0].VirtualAddress && a[1] <= @pe.sections[0].VirtualSize &&
+           a[2] == @pe.sections[1].VirtualAddress && a[3] <= @pe.sections[0].VirtualSize
+          )
+          found = true
+          break
+        end
+        pos -= 1
+      end
+      unless found
+        logger.debug "[.] failed to find obj_tbl"
+        return nil
+      end
+      logger.debug "[.] found possible obj_tbl at 0x%x" % pos
+
+      info = ASPACK_INFO.new
+      info.version = '?.?'
+      info.ObjTbl = pos
+      return info
+    end
     nil
   end
 
@@ -138,8 +260,15 @@ class PEdump::Packer::ASPack
   end
 
   def rebuild_imports ldr
-    return if @info.ImpTbl.to_i == 0
+    if @info.ImpTbl.to_i == 0
+      logger.error "[!] WARNING: no imports (ImpTbl=#{@info.ImpTbl.inspect})"
+      return
+    end
     rva = @ep_code[@info.ImpTbl,4].unpack('V').first
+    if rva == 0
+      logger.warn "[?] no imports? (rva=0)"
+      return
+    end
     logger.info "[.] imports rva=%6x" % rva
     unless io = ldr.va2stream(rva)
       logger.error "[!] va2stream(0x%x) FAIL" % rva
@@ -159,7 +288,11 @@ class PEdump::Packer::ASPack
   end
 
   def update_oep ldr
-    return if @info.OepOfs.to_i == 0
+    if @info.OepOfs.to_i == 0
+      ldr.pe_hdr.ioh.AddressOfEntryPoint = 0
+      logger.error "[!] WARNING: no entry point (OepOfs=#{@info.OepOfs.inspect})"
+      return
+    end
     rva = @ep_code[@info.OepOfs,4].unpack('V').first
     logger.info "[.] oep=%6x" % rva
     ldr.pe_hdr.ioh.AddressOfEntryPoint = rva
