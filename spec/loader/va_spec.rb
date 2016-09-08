bc5a30f0d2980f708c511dbd3d9f59045c2bf530
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2016-09-08 14:26:41 +0300

    loader/va_spec new rspec syntax

--- spec/loader/va_spec.rb
+++ spec/loader/va_spec.rb
@@ -11,13 +11,13 @@ describe PEdump::Loader do
 
       %w'1001000 1010000 104b999 104c000 1051000 109c000 10a01f5'.each do |x|
         it "returns true for 0x#{x}" do
-          @ldr.valid_va?(x.to_i(16)).should be_true
+          @ldr.valid_va?(x.to_i(16)).should be true
         end
       end
 
       %w'0 1 1000 1000fff 104b99a 104bfff 1050fff 109bfff 10a01f6'.each do |x|
         it "returns false for 0x#{x}" do
-          @ldr.valid_va?(x.to_i(16)).should be_false
+          @ldr.valid_va?(x.to_i(16)).should be false
         end
       end
     end
@@ -30,13 +30,13 @@ describe PEdump::Loader do
 
       %w'401000 541000 589000 589fff'.each do |x|
         it "returns true for 0x#{x}" do
-          @ldr.valid_va?(x.to_i(16)).should be_true
+          @ldr.valid_va?(x.to_i(16)).should be true
         end
       end
 
       %w'0 1 1000 400000 58a000'.each do |x|
         it "returns false for 0x#{x}" do
-          @ldr.valid_va?(x.to_i(16)).should be_false
+          @ldr.valid_va?(x.to_i(16)).should be false
         end
       end
     end
