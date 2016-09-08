22581506577647a7ab832e323f3a9599e89126d7
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2016-09-08 14:17:08 +0300

    fix "`example` is not available from within an example" rspec error

--- spec/support/samples.rb
+++ spec/support/samples.rb
@@ -3,14 +3,7 @@ SAMPLES_DIR = File.expand_path(File.dirname(__FILE__) + '/../../samples/')
 def sample
   @pedump ||=
     begin
-      fname =
-        if self.example
-          # called from it(...)
-          self.example.full_description.split.first
-        else
-          # called from before(:all)
-          self.class.metadata[:example_group][:description_args].first
-        end
+      fname = self.class.description
       fname = File.join(SAMPLES_DIR, fname)
       File.open(fname,"rb") do |f|
         if block_given?
