76a45f7fd7f1c857cc35879b20db523ebdf46849
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2022-01-29 22:26:58 +0200

    fix yaml load

--- spec/sections_spec.rb
+++ spec/sections_spec.rb
@@ -5,7 +5,20 @@ require 'yaml'
 ['calc.exe', 'bad/data_dir_15_entries.exe'].each do |fname|
   describe fname do
     it "should match saved sections info" do
-      sample.sections.should == YAML::load_file(File.join(DATA_DIR,"#{File.basename(fname)}_sections.yml"))
+      sections = nil
+      begin
+        # tested with ruby 3.0.3
+        sections = YAML::load_file(
+          File.join(DATA_DIR,"#{File.basename(fname)}_sections.yml"),
+          permitted_classes: [Symbol, PEdump::IMAGE_SECTION_HEADER]
+        )
+      rescue
+        # old ruby?
+        sections = YAML::load_file(
+          File.join(DATA_DIR,"#{File.basename(fname)}_sections.yml")
+        )
+      end
+      sample.sections.should == sections
     end
   end
 end
